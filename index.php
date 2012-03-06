985d5739f6aad022422e9858258888410a1f9087
Christian Jessen <christian@jessen.pe>
2012-03-06 15:05:03 -0300

    Updated missing vars for translation in index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ get_header();
 	    <div id="wp-post-list">
 		<?php while(have_posts()) : the_post(); ?>
 		<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><h2><?php the_title(); ?></h2></a>
-		<p class="timestamp">By <?php the_author_meta("first_name"); ?> <?php the_author_meta("last_name"); ?> - <?php the_date() ?> @ <?php the_time() ?> - <?php comments_popup_link('Comment on this &#187;', '1 Comment &#187;', '% Comments &#187;'); ?></p>
+		<p class="timestamp"><?php _e('Written by', 'bootstrap-for-wp'); ?> <?php the_author_meta("first_name"); ?> <?php the_author_meta("last_name"); ?> - <?php the_date() ?> @ <?php the_time() ?> - <?php comments_popup_link('Comment on this &#187;', '1 Comment &#187;', '% Comments &#187;'); ?></p>
 		<?php /*  - <?php the_category(', ') ?> */?>
 		<?php the_excerpt(); ?>
 
@@ -22,7 +22,7 @@ get_header();
 		<?php endwhile; ?>
 
 		<div class="posts_nav pagination">
-		<?php posts_nav_link(' &nbsp; ','Previous page','Next page'); ?>
+		<?php posts_nav_link(' &nbsp; ', __('Previous page', 'bootstrap-for-wp'), __('Next page', 'bootstrap-for-wp')); ?>
 		</div>
 	    </div>
 	<!-- End list of wordpress posts -->
