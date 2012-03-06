60076b895a3232e65e81a3a3a074034bd54dba17
Christian Jessen <christian@jessen.pe>
2012-03-06 14:38:01 -0300

    Updated functions.php to include the correct language file

--- functions.php
+++ functions.php
@@ -2,4 +2,13 @@
 if ( function_exists('register_sidebar') ) {
     register_sidebar(array('name'=>'Right-Sidebar', 'before_title' => '<h2>', 'after_title' => '</h2>'));
 }
+
+// Add functionality to include correct language files
+load_theme_textdomain( 'bootstrap-for-wp', TEMPLATEPATH.'/languages' );
+
+$locale = get_locale();
+$locale_file = (TEMPLATEPATH . "/languages/" . $locale . ".php");
+if ( is_readable($locale_file) ) {
+	require_once($locale_file);
+}
 ?>
